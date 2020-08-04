68ac389f978f458d7ed9517c285d76c88d023340
Gaston Mieres <xxx@xxx.local>
2020-08-04 13:56:38 -0400

    script trimmed

--- resources/js/script.js
+++ resources/js/script.js
@@ -6,7 +6,7 @@ const buttonCats = document.querySelector('.more-cats');
 const auth = config.MICKEY;
 
 $(document).ready(function() {
-    $('.js--section-features').waypoint(function(direction) {
+    $('.js--section-dogs').waypoint(function(direction) {
         // if (direction == "down") {
         //     $('nav').addClass('sticky');
         // } else {
@@ -73,35 +73,35 @@ $(document).ready(function() {
     
 
 // Nav bar scroll //
-    $(function() {
-        $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
-    });
+    // $(function() {
+    //     $('a[href*=#]:not([href=#])').click(function() {
+    //         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    //             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    //             if (target.length) {
+    //                 $('html,body').animate({
+    //                     scrollTop: target.offset().top
+    //                 }, 1000);
+    //                 return false;
+    //             }
+    //         }
+    //     });
+    // });
 
 // MOBILE NAV //
-$('.js--nav-icon').click(function() {
-    var nav = $('.js--main-nav');
-    var icon = $('.js--nav-icon i');
+// $('.js--nav-icon').click(function() {
+//     var nav = $('.js--main-nav');
+//     var icon = $('.js--nav-icon i');
     
-    nav.slideToggle(200);
+//     nav.slideToggle(200);
     
-    if (icon.hasClass('ion-navicon-round')) {
-        icon.addClass('ion-close-round');
-        icon.removeClass('ion-navicon-round');
-    } else {
-        icon.addClass('ion-navicon-round');
-        icon.removeClass('ion-close-round');
-    }        
-});
+//     if (icon.hasClass('ion-navicon-round')) {
+//         icon.addClass('ion-close-round');
+//         icon.removeClass('ion-navicon-round');
+//     } else {
+//         icon.addClass('ion-navicon-round');
+//         icon.removeClass('ion-close-round');
+//     }        
+// });
 });
 
 
